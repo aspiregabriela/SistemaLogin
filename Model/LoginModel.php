441eed30754c990780bc50ebfc8fa13849c40c2d
Gabriela Martins <aspiregabriela@gmail.com>
2022-10-17 19:35:38 -0300

    .

---
+++ Model/LoginModel.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Model;
+
+use Sistema Login\DAO\LoginDAO;
+
+class LoginModel extends Model
+{
+   
+    public $id, $nome, $email, $senha;
+    
+    public function autenticar()
+    {
+        $dao = new LoginDAO();
+        
+        $dados_usuario_logado = $dao->selectByEmailAndSenha($this->email, $this->senha);
+
+        if(is_object($dados_usuario_logado))
+            return $dados_usuario_logado;
+        else
+            null;
+    }
+}
